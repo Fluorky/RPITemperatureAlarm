e023458242618d571438638edb7c1e86587053c5
Fluorky <93129147+Fluorky@users.noreply.github.com>
2023-03-16 23:15:53 +0100

    Update TemperatureAlarm.py
    Use with open() instead open() and close()
    Use strftime

--- TemperatureAlarm.py
+++ TemperatureAlarm.py
@@ -1,28 +1,38 @@
 import time
 import w1thermsensor
 import RPi.GPIO as GPIO
+from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(21, GPIO.OUT)
-GPIO.output(21, GPIO.HIGH)
 GPIO.setup(17,GPIO.OUT)
 GPIO.setup(26,GPIO.OUT)
+GPIO.output(21, GPIO.HIGH)
 GPIO.output(26,GPIO.LOW)
 GPIO.output(17,GPIO.LOW)
-tmax = 29.0
-tmin = 2.0
-sensor = w1thermsensor.W1ThermSensor()
 
-logs = open('logs.txt', 'w')
-for i in range (0,60):
-    temp=sensor.get_temperature()
-    if temp > tmax:
+class DefaultValues:
+    def __init__(self):
+        self.tmax = 29.0
+        self.tmin = 2.0
+        self.sensor = w1thermsensor.W1ThermSensor()
+        
+class DateWithTime:
+    def __init__(self):
+        self.now = datetime.now()
+        self.dateTime = self.now.strftime("%m/%d/%Y %H:%M:%S")
+
+with open('logs.txt', 'w') as logs:
+ for i in range (0,10):#60):
+    dv=DefaultValues()
+    temp=dv.sensor.get_temperature()
+    if temp > dv.tmax:
         GPIO.output(21, GPIO.LOW)
         GPIO.output(26, GPIO.HIGH)
         print("HOT")
         logs.write('HOT!')
-    elif temp < tmin:
+    elif temp < dv.tmin:
         GPIO.output(21, GPIO.LOW)
         GPIO.output(17, GPIO.HIGH)
         print("ICE ALERT")
@@ -32,42 +42,14 @@ for i in range (0,60):
         GPIO.output(17, GPIO.LOW)
         GPIO.output(26, GPIO.LOW)
         
-    tempera=str(temp)
-    timer=time.localtime()
-    h=str(timer[3])
-    m=str(timer[4])
-    s=str(timer[5])
-    logs.write(tempera)
-    print(tempera, end="")
-    logs.write('    ')
-    print('    ', end="")
-    if int(h)<10:
-     logs.write('0'+h)
-     print('0'+h, end="")
-    else:
-     logs.write(h)
-     print(h, end="")
-    logs.write(':')
-    print(':', end="")
-    if int(m)<10:
-     logs.write('0'+m)
-     print('0'+m, end="")
-    else:
-     logs.write(m)
-     print(m, end="")
-    logs.write(':')
-    print(':', end="")
-    if int(s)<10:
-     logs.write('0'+s)
-     print('0'+s, end="")
-    else:
-     logs.write(s)
-     print(s, end="")
-    logs.write('\n')
-    print('\n')
+    temper=str(temp)
+    dwt = DateWithTime()
+    mess = dwt.dateTime
+    
+    logs.write(mess+" "+temper+'\n')
+    print(mess,temper, end="\n")
     time.sleep(1)
 
-logs.close()
 GPIO.output(21, GPIO.HIGH)
 GPIO.output(26, GPIO.LOW)
 GPIO.output(17, GPIO.LOW)
